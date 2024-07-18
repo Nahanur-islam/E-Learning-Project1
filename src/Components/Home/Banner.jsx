9f47f851675c69e4ac8bfa83df44cc687ceb579f
Nahanur-islam <nahnanurism71@gmail.com>
2024-07-18 12:53:37 +0600

    first commit

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,26 @@
+import Mask_group from '../../../public/Mask_group.png'
+
+const Banner = () => {
+    return (
+        <div className='flex'>
+           
+            <div className='w-1/2  text-black  mt-28'>
+                <h1 className='text-3xl font-bold py-4'>Bringing the taste of your state at your doorstep..!</h1>
+
+                <p className=''>Get the hot and freshly cooked food of your state delivered at your doorstep with the taste you deserve, in just no time.</p>
+            
+                
+                <div className='mr-4'>
+                    <button className='btn bg-red-500'>Flower Order Now</button>
+                    <button className='btn bg-red-500'>Flower Details</button>
+              </div>
+            </div>
+
+            <div className=' right-0 w-1/2 '>
+                <img src={Mask_group} className='w-full h-[550px] object-cover' alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,49 @@
+
+import { NavLink } from 'react-router-dom';
+import Flower1 from '../../public/Flower1.png'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-around py-6 '>
+            <div>
+                <img src={Flower1} className='w-20 h-16 object-cover' alt="" />
+            </div>
+            <div className=' my-auto'>
+                <ul className='flex gap-4'>
+                    <NavLink to={'/'} className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }>
+                        <li className='text-lg font-semibold'>Home</li>
+                    </NavLink>
+                    <NavLink to={'menu'} className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }>
+                        <li className='text-lg font-semibold'>Menu</li>
+                    </NavLink>
+                    <NavLink to={'contact'} className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }>
+                        <li className='text-lg font-semibold'>Contact</li>
+                    </NavLink>
+                    <NavLink to={'services'} className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }>
+                        <li className='text-lg font-semibold'>Services</li>
+                    </NavLink>
+                    <NavLink to={'about us'} className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }>
+                        <li className='text-lg font-semibold'>About Us</li>
+                    </NavLink>
+                </ul>
+            </div>
+            <div className=' my-auto'>
+                <button className='btn bg-red-500'>
+                    Login
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
